5f5f98555e29d313474334f3bba91621f285e222
VISHNU PRIYAN <116505167+VichuVS@users.noreply.github.com>
2022-11-22 19:18:03 +0530

    Update Vehicle.java

--- Vehicle.java
+++ Vehicle.java
@@ -3,8 +3,8 @@ record Vehicle(String Car_name, String Fuel_type, int Cost) {
 	Vehicle() {
 this("A","Diesel",500000); }
 public static void main(String ar[]) {
-	Vehicle v=new Vehicle("Swift","Diesel",700000);
-	Vehicle v1=new Vehicle("Sonet","Diesel",800000);
+	Vehicle v=new Vehicle("Maruti","Diesel",700000);
+	Vehicle v1=new Vehicle("Kia","Diesel",800000);
 	Vehicle v2=new Vehicle("BMW","Diesel",6000000);
 	Vehicle v3=new Vehicle("AUDI","Diesel",5500000);
 System.out.println(v);
